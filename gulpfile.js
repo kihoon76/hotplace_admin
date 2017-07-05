2980df10139551a9e3c144236a8038b79c0bc3a3
hoon <lovedeer118@gmail.com>
2017-07-05 14:49:51 +0900

    gulp 설정

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,38 @@
-var gulp = require('gulp');
-var extjs = require('gulp-uglify');
+var gulp   = require('gulp'),
+	uglify = require('gulp-uglify'),
+	cssMin =require('gulp-cssmin'),
+	gutil  = require('gulp-util'),
+	del	   = require('del'),
+	stripDebug = require('gulp-strip-debug');
 
-gulp.task('scripts', function() {
-    return gulp.src(['public/core/ver/4.1.1-rc2/js/ext-all.js', 'public/app.js'])
-               .pipe(extjs())
-               .pipe(gulp.dest('build'));
+var DIR = {
+	SRC  : 'public/src',
+	DIST : 'public/dist'
+}
+
+var SRC = {
+	JS  : DIR.SRC + '/**/*.js',
+	CSS : DIR.SRC + '/**/*.css'  
+}
+
+gulp.task('js-minify', function() {
+	gutil.log('자바스크립트 minify');
+	return  gulp.src(SRC.JS)
+			.pipe(stripDebug())
+			.pipe(uglify())
+			.pipe(gulp.dest(DIR.DIST));
 });
+
+gulp.task('css-minify', function() {
+	gutil.log('스타일시트 minify');
+	return  gulp.src(SRC.CSS)
+			.pipe(cssMin())
+			.pipe(gulp.dest(DIR.DIST));
+});
+
+gulp.task('clean', function() {
+	gutil.log('dist 디렉토리 내용삭제');
+	return del.sync([DIR.DIST + '/*']);
+});
+
+gulp.task('default', ['clean', 'js-minify', 'css-minify']);
